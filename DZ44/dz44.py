e8e4bd2b2bd68f6f99a8433ea243da5a3ac29787
NZpass <snake_28@mail.ru>
2023-12-17 13:09:54 +0300

    решение

--- DZ44/dz44.py
+++ DZ44/dz44.py
@@ -2,6 +2,8 @@
 # которая состоит всего из 1 столбца. Ваша задача перевести его в one hot вид.
 # Сможете ли вы это сделать без get_dummies?
 
+# Даны условия задачи
+
 import pandas as pd
 import random
 lst = ['robot'] * 10
@@ -9,3 +11,17 @@ lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI':lst})
 data.head()
+
+# Вывод рандом списка робот-человек, но можно и без него.
+print(data)
+
+# Решение
+
+data = pd.DataFrame({'whoAmI':lst})
+data.loc[data['whoAmI'] == 'robot', 'Robots'] = '1'
+data.loc[data['whoAmI'] != 'robot', 'Robots'] = '0'
+data.loc[data['whoAmI'] == 'human', 'Humans'] = '1'
+data.loc[data['whoAmI'] != 'human', 'Humans'] = '0'
+
+# Вывод таблицы робот-человек
+print(data)
